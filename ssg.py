1a60cf511e206a6d68d9db7b99d7249781358ebd
Martti Roponen <martti.roponen@gmail.com>
2022-01-29 21:21:14 +0200

    The Site Class changes

---
+++ ssg.py
@@ -0,0 +1,11 @@
+import typer
+import ssg.site
+
+
+def main(source="content", dest="dist"):
+    config = {'source': source, 'dest': dest}
+    site = ssg.site.Site(**config)
+    site.build()
+
+
+typer.run(main())
